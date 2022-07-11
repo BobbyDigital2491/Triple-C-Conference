75d5982848247f0c7ab16fd473749ca63be2f1ac
BobbyDigital2491 <100319461+BobbyDigital2491@users.noreply.github.com>
2022-07-10 21:04:40 -0400

    Update constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-export const SITE_URL = 'Triple C University';
+export const SITE_URL = 'https://triple-c-conference.vercel.app/';
 export const SITE_ORIGIN = process.env.NEXT_PUBLIC_SITE_ORIGIN || new URL(SITE_URL).origin;
 export const TWITTER_USER_NAME = 'triple c';
 export const BRAND_NAME = 'Triple C';
