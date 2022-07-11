7ee62a12b2b0eaf185b631ba3139925200077908
BobbyDigital2491 <100319461+BobbyDigital2491@users.noreply.github.com>
2022-07-10 21:06:57 -0400

    Update learn-more.tsx

--- components/learn-more.tsx
+++ components/learn-more.tsx
@@ -22,7 +22,7 @@ import { REPO } from '@lib/constants';
 export default function LearnMore() {
   return (
     <div className={cn(styleUtils.appear, styleUtils['appear-fifth'], styles.contact)}>
-      Learn more on{' '}
+      Visit us on{' '}
       <a href={REPO} className={styles['contact-email']} target="_blank" rel="noopener noreferrer">
         GitHub
       </a>
